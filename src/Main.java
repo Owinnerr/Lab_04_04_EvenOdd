0dca91c96e4bcf838da3d041e606ea46b24ca0bb
Owin Abeysinghe <owinabeysinghe@gmail.com>
2024-02-03 17:36:01 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int numToExamine = 4; // Initialize the variable to use for even or odd testing
+        if (numToExamine % 2 == 0) { // Condition: If the remainder of the number divided by 2 is 0
+            System.out.println("Even"); // Output even if the condition is true
+        } else {
+            System.out.println("Odd"); // Output odd if condition is not true
+        }
+    }
+}
